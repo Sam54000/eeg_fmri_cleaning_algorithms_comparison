41abc701cc35bf00d5983f84b187402510309146
Samuel Louviot <sam.louviot@gmail.com>
2024-04-04 11:18:38 -0400

    wrapping up the main

--- src/eeg_fmri_cleaning_algorithms_comparison/main.py
+++ src/eeg_fmri_cleaning_algorithms_comparison/main.py
@@ -1,3 +1,35 @@
+#!/usr/bin/env -S  python  #
+# -*- coding: utf-8 -*-
+# ===============================================================================
+# Author: Dr. Samuel Louviot, PhD
+# Institution: Nathan Kline Institute
+#              Child Mind Institute
+# Address: 140 Old Orangeburg Rd, Orangeburg, NY 10962, USA
+#          215 E 50th St, New York, NY 10022
+# Date: 2024-04-04
+# email: samuel DOT louviot AT nki DOT rfmh DOT org
+# ===============================================================================
+# LICENCE GNU GPLv3:
+# Copyright (C) 2024  Dr. Samuel Louviot, PhD
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# ===============================================================================
+
+"""Cleaning pipelines for EEG data.
+
+This module contains several methods to clean the EEG data recorded during fMRI.
+The different methods consist of cleaning the gradient and BCG artifacts with 
+the homemade pipelines (called here CBIN-CLEANER). Once this first step is done,
+the data can be further cleaned by either using ASR and/or PyPrep algorithms.
+"""
 
 import os
 import shutil
@@ -8,135 +40,172 @@ import bids
 import mne
 import numpy as np
 import pyprep
-from bids.layout import BIDSLayout
 from eeg_fmri_cleaning.main import clean_bcg, clean_gradient
 from eeg_fmri_cleaning.utils import read_raw_eeg
-from EEG_quality_assessment.signal_metrics import SignalMetrics
 
-from .utils import (
-    asr_cleaner,
-    cbin_cleaner,
-    copy_sidecar,
-    is_task_checker,
-    make_directory,
-    pyprep_cleaner,
-    write_report,
-)
 
-data_path = '/projects/EEG_FMRI/bids_eeg/BIDS/NEW/RAW'
-derivatives_path = '/projects/EEG_FMRI/bids_eeg/BIDS/NEW/DERIVATIVES'
-
-layout = BIDSLayout(data_path)
-file_list = layout.get(return_type='file', extension='.set')
-
-def cbin_cleaner_pipeline(file: str | os.PathLike, 
-                          derivatives_path: str | os.PathLike, 
-                          saving_path_name: str | os.PathLike) -> mne.io.Raw:
-    """Combine the cbin_cleaner function with side stuff to save the file.
+def write_report(message: str, filename: str | os.PathLike) -> None:
+    """Append a message to a txt file.
 
     Args:
-        file (str | os.PathLike): The file name to clean
-        derivatives_path (str | os.PathLike): The path to save the cleaned file
-        saving_path_name (str | os.PathLike): The name of the folder to save 
-                                              the cleaned file
-
-    Returns:
-        mne.io.Raw: The cleaned file
+        message (str): The message to append.
+        filename (str | os.PathLike): The file to append the message.
     """
-    raw = cbin_cleaner(file)
-    saving_file_path_cbin_cleaner = make_directory(
-        file, 
-        os.path.join(derivatives_path, saving_path_name)
-                        )
+    with open(filename, "a") as f:
+        f.write(message)
+        f.write("\n")
+
+class CleanerPipelines:
+    def __init__(self, BIDSFile: bids.layout.models.BIDSFile) -> None:  # noqa: D107
+        self.BIDSFile = BIDSFile
+        self.entities = BIDSFile.get_entities()
+        self.rawdata_path = Path(BIDSFile.path)
+        self.process_history = []
+
+    def _read_raw(self: "CleanerPipelines") -> "CleanerPipelines":
+        """Read the raw EEG data using MNE."""
+        self.raw = read_raw_eeg(self.BIDSFile.path)
+        return self
+
+    def _save_raw(self: "CleanerPipelines") -> "CleanerPipelines":
+        """Save the cleaned raw EEG data in the BIDS format."""
+        self._make_derivatives_saving_path()
+        self.raw.save(self.derivatives_path)
+        return self
+
+    def _make_derivatives_saving_path(
+        self: "CleanerPipelines") -> "CleanerPipelines":
+        """Create the path to save the cleaned files in the BIDS format.
+
+        It is a file specific path that is generated based on the BIDSFile
+        object.
+
+        Args:
+            added_folder (str, optional): The folder to be added after the 
+                                          derivatives one.
+        """
+        reading_path = Path(self.BIDSFile.path)
+        path_parts = list(reading_path.parts)
+        rawdata_dirname = [name for name in path_parts if "raw" in name.lower()][0]
+        path_parts[path_parts.index(rawdata_dirname)] = "DERIVATIVES"
+        added_folder = "_".join(self.process_history)
+        if added_folder:
+            path_parts.insert(path_parts.index("DERIVATIVES") + 1, added_folder)
+            
+        self.derivatives_path = Path(*path_parts)
+        self.derivatives_path.mkdir(parents=True, exist_ok=True)
+        return self
+
+    def _copy_sidecar(self: "CleanerPipelines") -> None:
+        """Copy the sidecar file to the derivative folder.
+
+        Args:
+            BIDSFile (bids.layout.models.BIDSFile): The BIDSFile object.
+            where_to_copy (str | os.PathLike): The folder to copy the sidecar file.
+        """
+        filename_base, _ = os.path.splitext(self.BIDSFile.path)
+        sidecar_filename = filename_base + ".json"
+
+        if not hasattr(self, "derivatives_path"):
+            self._make_derivatives_saving_path()
+
+        if os.path.isfile(sidecar_filename):
+            shutil.copyfile(sidecar_filename, self.derivatives_path)
+
+    def _clean_gradient(self: "CleanerPipelines") -> "CleanerPipelines":
+        """Clean the gradient artifacts from the EEG data."""
+        self.raw = clean_gradient(self.raw)
+        return self
+
+    def _clean_bcg(self: "CleanerPipelines") -> "CleanerPipelines":
+        """Clean the BCG artifacts from the EEG data."""
+        self.raw = clean_bcg(self.raw)
+        return self
+
+    def _run_cbin_cleaner(self, task_condition: str = "checker") -> mne.io.Raw:
+        """Clean the BCG and gradient artifacts from the EEG data.
+
+        Args:
+            BIDSFile (bids.layout.models.BIDSFile): The BIDSFile object.
+            task_condition (str, optional): Determine on which task the
+                                            gradient artifacts should be cleaned.
+
+        Returns:
+            mne.io.Raw: The cleaned EEG data.
+        """
+        if self.BIDSFile.get_entities()["task"] == task_condition:
+            self._clean_gradient()
+        self._clean_bcg()
+        self.process_history.append("CBIN-CLEANER")
+        return self
+
+    def _run_pyprep(self: "CleanerPipelines") -> "CleanerPipelines":
+        """Clean the EEG data using the PyPrep algorithm.
+
+        Args:
+            raw (mne.io.Raw): The raw EEG data.
+
+        Returns:
+            mne.io.Raw: The cleaned EEG data.
+        """
+        montage = mne.channels.make_standard_montage("easycap-M10")
+        self.raw.apply_montage(montage)
+        prep_params = {
+            "ref_chs": "eeg",
+            "reref_chs": "eeg",
+            "line_freqs": np.arange(60, self.raw.info["sfreq"] / 2, 60),
+        }
+        prep = pyprep.PrepPipeline(self.raw, prep_params, channel_wise=True)
+        prep.fit()
+        self.raw = prep.raw
+        self.process_history.append("PREP")
+        return self
+
+    def _run_asr(self) -> mne.io.Raw:
+        """Clean the EEG data using the ASR algorithm.
+
+        Args:
+            raw (mne.io.Raw): The raw EEG data.
+
+        Returns:
+            mne.io.Raw: The cleaned EEG data.
+        """
+        asr = asrpy.ASR()
+        asr.fit(self.raw)
+        self.raw = asr.transform(self.raw)
+        self.process_history.append("ASR")
+        return self
+
+    def run_cbin_pipeline(self: "CleanerPipelines") -> "CleanerPipelines":
+        """Run the pipeline to clean the EEG data using the CBIN algorithm."""
+        self._run_cbin_cleaner()
+        self._make_derivatives_saving_path()
+        self._save_raw()
+        self._copy_sidecar()
+        return self
     
-    raw.save(os.path.join(
-        saving_file_path_cbin_cleaner,
-        os.path.split(file)[1]
-        )
-    )
-    copy_sidecar(file, saving_file_path_cbin_cleaner)
-    return raw
-
-def pyprep_cleaner_pipeline(raw: mne.io.Raw,
-                            reading_filename: str | os.PathLike,
-                            derivatives_path: str | os.PathLike, 
-                            saving_path_name: str | os.PathLike) -> mne.io.Raw:
-    """Applies the PyPREP cleaner algorithm.
-
-    Args:
-        raw (mne.io.Raw): The raw EEG data to be cleaned.
-        derivatives_path (str|os.PathLike): The path to the directory where 
-                                            the cleaned data will be saved.
-        saving_path_name (str|os.PathLike): The name of the file to be saved.
-
-    Returns:
-        mne.io.Raw: The cleaned raw EEG data.
-    """
-    raw = pyprep_cleaner(raw)
-    saving_file_path_prep = make_directory(
-        reading_filename, 
-        os.path.join(derivatives_path, saving_path_name)
-    )
+    def run_asr_pipeline(self: "CleanerPipelines") -> "CleanerPipelines":
+        """Run the pipeline to clean the EEG data using the ASR algorithm."""
+        self._run_asr()
+        self._save_raw()
+        self._copy_sidecar()
+        return self
     
-    raw.save(os.path.join(
-        saving_file_path_prep,
-        os.path.split(reading_filename)[1]
-    ))
-    copy_sidecar(reading_filename, saving_file_path_prep)
-    return raw
+    def run_prep_pipeline(self: "CleanerPipelines") -> "CleanerPipelines":
+        """Run the pipeline to clean the EEG data using the PyPrep algorithm."""
+        self._run_pyprep()
+        self._save_raw()
+        self._copy_sidecar()
+        return self
 
-def asr_cleaner_pipeline(raw: mne.io.Raw,
-                         reading_filename: str | os.PathLike,
-                         derivatives_path: str | os.PathLike, 
-                         saving_path_name: str | os.PathLike) -> mne.io.Raw:
-    """Applies the ASR cleaner algorithm.
+if __name__ == "__main__":
+    data_path = "/projects/EEG_FMRI/bids_eeg/BIDS/NEW/RAW"
 
-    Args:
-        raw (mne.io.Raw): The raw EEG data to be cleaned.
-        reading_filename (str | os.PathLike): The name of the file to be cleaned.
-        derivatives_path (str | os.PathLike): The path to the directory where
-                                              the cleaned data will be saved.
-        saving_path_name (str | os.PathLike): The name of the file to be saved.
+    layout = bids.BIDSLayout(data_path)
+    file_list = layout.get(extension=".set")
 
-    Returns:
-        mne.io.Raw: The cleaned raw EEG data.
-    """
-    raw = asr_cleaner(raw)
-    saving_file_path_asr = make_directory(
-        reading_filename, 
-        os.path.join(derivatives_path, derivatives_path)
-                        )
-    
-    raw.save(os.path.join(
-        saving_file_path_asr,
-        os.path.split(reading_filename)[1]
-        )
-    )
-    copy_sidecar(reading_filename, saving_file_path_asr)
-    return raw
-
-def main():
-    for file in file_list:
-        if is_task_checker(file):
-            try:
-                raw = cbin_cleaner_pipeline(file, derivatives_path, 'CBIN_CLEANER')
-                raw_copy = raw.copy()
-                raw = asr_cleaner_pipeline(raw_copy, 
-                                           derivatives_path, 
-                                           'CBIN_CLEANER_ASR')
-                raw = pyprep_cleaner_pipeline(raw, 
-                                              derivatives_path, 
-                                              'CBIN_CLEANER_PREP')
-                raw = asr_cleaner_pipeline(raw, 
-                                           derivatives_path, 
-                                           'CBIN_CLEANER_PREP_ASR')
-                
-                write_report(file,
-                    f"{derivatives_path}/processed.txt")
-            except Exception as e:
-                write_report(f"{file}: {str(e)}", 
-                    f"{derivatives_path}/not_processed.txt")
-                continue
-
-if __name__ == '__main__':
-    main()
+    for filename, BIDSFile_object in file_list.items():
+        cleaner = CleanerPipelines(BIDSFile_object)
+        cleaner._read_raw()
+        cleaner.run_cbin_pipeline().run_asr_pipeline()
+        cleaner._read_raw().run_cbin_pipeline().run_prep_pipeline().run_asr_pipeline()
